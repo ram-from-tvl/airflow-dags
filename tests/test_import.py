9e97a87fca943f1310dd30466768472ff54101b5
devsjc <47188100+devsjc@users.noreply.github.com>
2025-03-02 22:15:59 +0000

    feat(repo): Add basic test

---
+++ tests/test_import.py
@@ -0,0 +1,26 @@
+import unittest
+from importlib.resources import files
+
+import airflow.models
+
+
+class TestImport(unittest.TestCase):
+    """Validate DAG files using airflow's DagBag.
+
+    This includes sanity checks e.g. do tasks have required arguments,
+    are DAG ids unique & do DAGs have no cycles.
+    """
+
+    def test_import_dags_without_errors(self) -> None:
+        dag_bag = airflow.models.DagBag(
+            include_examples=False,
+            dag_folder=str(files("airflow_dags").joinpath("dags")),
+        )
+        self.assertEqual(len(dag_bag.dags), 23)
+        self.assertFalse(dag_bag.import_errors)
+
+        # Additional project-specific checks can be added here, e.g. to enforce each DAG has a tag
+        for dag_id, dag in dag_bag.dags.items():
+            error_msg = f"{dag_id} in {dag.full_filepath} has no tags"
+            self.assertTrue(len(dag.tags) == 0, msg=error_msg)
+
