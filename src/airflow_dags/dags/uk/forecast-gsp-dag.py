b7bd2ae278a9ecbd6165d56716bdb0ef6cc51334
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-03-20 13:52:13 +0000

    50 pvnet log (#52)
    * update message, and always trigger blend
    
    * lint
    
    * change to use one_done

--- src/airflow_dags/dags/uk/forecast-gsp-dag.py
+++ src/airflow_dags/dags/uk/forecast-gsp-dag.py
@@ -92,7 +92,7 @@ with DAG(
         },
         task_concurrency=10,
         on_failure_callback=slack_message_callback(forecast_blend_error_message),
-        trigger_rule="always",
+        trigger_rule="one_done",
         awslogs_group="/aws/ecs/blend/forecast_blend",
         awslogs_stream_prefix="streaming/forecast_blend-blend",
         awslogs_region="eu-west-1",
