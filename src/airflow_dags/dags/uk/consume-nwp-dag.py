d22021d78dca5204cbe2b10df0ab7820dbcf5122
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-04-02 16:32:52 +0100

    Nwp 1.1.16 (#131)
    * nwp 1.1.16
    
    * 1.1.17
    
    * 1.1.18
    
    * nwp 1.1.19

--- src/airflow_dags/dags/uk/consume-nwp-dag.py
+++ src/airflow_dags/dags/uk/consume-nwp-dag.py
@@ -30,7 +30,7 @@ env = os.getenv("ENVIRONMENT", "development")
 nwp_consumer = ContainerDefinition(
     name="nwp-consumer",
     container_image="ghcr.io/openclimatefix/nwp-consumer",
-    container_tag="1.1.18",
+    container_tag="1.1.19",
     container_env={
         "CONCURRENCY": "false",
         "LOGLEVEL": "DEBUG",
