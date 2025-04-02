c80b48520461ad01b428501c8b758a5228671eb0
devsjc <47188100+devsjc@users.noreply.github.com>
2025-04-02 12:26:45 +0100

    Update consume-nwp-dag.py (#117)

--- src/airflow_dags/dags/uk/consume-nwp-dag.py
+++ src/airflow_dags/dags/uk/consume-nwp-dag.py
@@ -44,7 +44,7 @@ nwp_consumer = ContainerDefinition(
     },
     container_command=["consume"],
     container_cpu=512,
-    container_memory=2048,
+    container_memory=3072,
 )
 
 def update_operator(provider: str) -> BashOperator:
