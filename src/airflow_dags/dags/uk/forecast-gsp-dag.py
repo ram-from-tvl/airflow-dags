91b19299e17996cce0fbc548a7406048492170bd
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-05-14 18:40:06 +0100

    pin gsp intraday to "2.5.18" on production (#201)

--- src/airflow_dags/dags/uk/forecast-gsp-dag.py
+++ src/airflow_dags/dags/uk/forecast-gsp-dag.py
@@ -54,7 +54,7 @@ gsp_forecaster = ContainerDefinition(
 dev_gsp_intraday_forecaster = ContainerDefinition(
     name="forecast-pvnet",
     container_image="ghcr.io/openclimatefix/uk-pvnet-app",
-    container_tag="cloudcasting_inputs",
+    container_tag="cloudcasting_inputs" if env == "development" else "2.5.18",
     container_env={
         "LOGLEVEL": "INFO",
         "RAISE_MODEL_FAILURE": "critical",
