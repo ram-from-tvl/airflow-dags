4e625457683ec16084b91dd08ce31919d4ef59c7
Megawattz <47150349+zakwatts@users.noreply.github.com>
2025-05-28 17:16:15 +0100

    Update nl forecast DAG ram (#213)

--- src/airflow_dags/dags/uk/forecast-site-nl-dag.py
+++ src/airflow_dags/dags/uk/forecast-site-nl-dag.py
@@ -36,7 +36,7 @@ site_forecaster = ContainerDefinition(
         f"{env}/rds/pvsite": ["DB_URL"],
     },
     container_cpu=1024,
-    container_memory=3072,
+    container_memory=4096,
     domain="nl",
 )
 
