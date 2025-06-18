389deb232a9531c8e417ed4af600a5985506576b
devsjc <47188100+devsjc@users.noreply.github.com>
2025-06-18 07:19:35 +0100

    feat(uk): Revert development to 1.6.9 (#242)
    * chore(README): Add 1.7.0 release notes
    
    * feat(uk): Return development to v1.6.9

--- src/airflow_dags/dags/uk/forecast-site-nl-dag.py
+++ src/airflow_dags/dags/uk/forecast-site-nl-dag.py
@@ -27,7 +27,7 @@ default_args = {
 site_forecaster = ContainerDefinition(
     name="forecast-site-nl",
     container_image="ghcr.io/openclimatefix/site-forecast-app",
-    container_tag="0.0.11",
+    container_tag="0.0.18",
     container_env={
         "NWP_ECMWF_ZARR_PATH":f"s3://nowcasting-nwp-{env}/ecmwf-nl/data/latest.zarr",
         "SATELLITE_ZARR_PATH":f"s3://nowcasting-sat-{env}/data/latest/latest.zarr.zip",
