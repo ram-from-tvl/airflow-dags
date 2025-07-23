87cdb0dd5d89343a1b4be5a0c05bb9baba8a6b02
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-07-23 12:11:23 +0100

    dont check for generation (#280)
    * dont check for generation
    
    * fix
    
    * check 36 hours
    
    * Just check for 30 hours and add check

--- src/airflow_dags/dags/uk/check-api-site.py
+++ src/airflow_dags/dags/uk/check-api-site.py
@@ -1,5 +1,4 @@
-
-"""General checks on Uk National/GSP API."""
+"""General checks on Uk Site API."""
 
 import datetime as dt
 import logging
@@ -75,42 +74,12 @@ def check_forecast(access_token: str) -> None:
         check_key_in_data(data, "forecast_values")
         forecast_values = data["forecast_values"]
 
-        # should have data point for 2 days in the past + 36 hours in the future
+        # should have data point for 2 days in the past + 36 hours in the future.
+        # We just check for the next 30 hours though
         # date is in 30 min intervals
         check_len_ge(forecast_values, 2 * 24 * 2 + 30 * 2)
         check_key_in_data(forecast_values[0], "target_datetime_utc")
         check_key_in_data(forecast_values[0], "expected_generation_kw")
-
-
-def check_generation(access_token: str) -> None:
-    """Check the forecast."""
-    sites = call_api(url=f"{base_url}/sites", access_token=access_token)
-    for site in sites["site_list"]:
-        site_uuid = site["site_uuid"]
-
-        full_url = f"{base_url}/sites/{site_uuid}/pv_actual"
-
-        data = call_api(url=full_url, access_token=access_token)
-
-        check_key_in_data(data, "pv_actual_values")
-        pv_actual_values = data["pv_actual_values"]
-
-        # should have data point for 2 days in the past
-        # date is in 30 min intervals
-        check_len_ge(pv_actual_values, 2 * 24 * 4)
-        check_key_in_data(pv_actual_values[0], "datetime_utc")
-        check_key_in_data(pv_actual_values[0], "actual_generation_kw")
-
-        # check that the last datetime is within the last hour
-        last_datetime = max([d["datetime_utc"] for d in pv_actual_values])
-        # convert last_datetime to a datetime object
-        last_datetime = dt.datetime.fromisoformat(last_datetime).replace(tzinfo=dt.UTC)
-        if dt.datetime.now(dt.UTC) - last_datetime > dt.timedelta(hours=1):
-            raise ValueError(
-                f"Last datetime {last_datetime} is more than 1 hour old. "
-                "This is likely because the API has not been updated with the latest data."
-                f"This is for {site=}",
-            )
 
 
 @dag(
@@ -153,18 +122,12 @@ def api_site_check() -> None:
         op_kwargs={"access_token": access_token_str},
     )
 
-    generation = PythonOperator(
-        task_id="check-generation",
-        python_callable=check_forecast,
-        op_kwargs={"access_token": access_token_str},
-    )
-
     if_any_task_failed = PythonOperator(
         task_id="api-uk-national-gsp-check-if-any-task-failed",
         python_callable=lambda: None,
         trigger_rule="one_failed",
         on_success_callback=slack_message_callback(
-            "⚠️ One of the API Site checks has failed. 🇬🇧 "
+            "⚠️ 🇬🇧 {{ ti.dag_id }} One of the API Site checks has failed. "
             "See which ones have failed on airflow, to help debug the issue. "
             "No out-of-hours support is required.",
         ),
@@ -175,14 +138,12 @@ def api_site_check() -> None:
         >> sites
         >> [
             forecast,
-            generation,
         ]
     )
 
     [
         sites,
         forecast,
-        generation,
     ] >> if_any_task_failed
 
 
@@ -195,4 +156,3 @@ if __name__ == "__main__":
     bearer_token = get_bearer_token_from_auth0()
     check_sites(bearer_token)
     check_forecast(bearer_token)
-    check_generation(bearer_token)
