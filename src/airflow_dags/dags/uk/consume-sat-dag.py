c4feafadf8a2de20c2dfff167df4b0b4a6387ceb
Peter Dudfield <34686298+peterdudfield@users.noreply.github.com>
2025-08-06 17:39:01 +0100

    add 0 degree satellite (#304)
    * add 0 degree satellite
    
    * lint

--- src/airflow_dags/dags/uk/consume-sat-dag.py
+++ src/airflow_dags/dags/uk/consume-sat-dag.py
@@ -11,6 +11,7 @@ import os
 from airflow.decorators import dag
 from airflow.operators.bash import BashOperator
 from airflow.operators.latest_only import LatestOnlyOperator
+from airflow.utils.trigger_rule import TriggerRule
 
 from airflow_dags.plugins.callbacks.slack import get_task_link, slack_message_callback
 from airflow_dags.plugins.operators.ecs_run_task_operator import (
@@ -145,30 +146,39 @@ def sat_consumer_dag() -> None:
         cadence_mins=5,
     )
 
-    # consume_iodc_op = EcsAutoRegisterRunTaskOperator(
-    #    airflow_task_id="consume-odegree",
-    #     container_def=sat_consumer,
-    #     trigger_rule=TriggerRule.ALL_FAILED, # Only run if rss fails
-    #     env_overrides={
-    #         "SATCONS_SATELLITE": "odegree",
-    #         "SATCONS_TIME": "{{" \
-    #             + "(data_interval_start - macros.timedelta(minutes=210))" \
-    #             + ".strftime('%Y-%m-%dT%H:%M')" \
-    #             + "}}",
-    #         "SATCONS_WORKDIR": f"s3://nowcasting-sat-{env}/testdata/iodc",
-    #     },
-    #     on_failure_callback=slack_message_callback(
-    #         "⚠️ The task {{ ti.task_id }} failed to collect odegree satellite data. "
-    #         "The forecast will automatically move over to PVNET-ECMWF "
-    #         "which doesn't need satellite data. "
-    #         "Forecast quality may be impacted, "
-    #         "but no out-of-hours support is required. "
-    #         "Please log in an incident log. ",
-    #     ),
-    # )
+    consume_odegree_op = EcsAutoRegisterRunTaskOperator(
+        airflow_task_id="consume-odegree",
+        container_def=sat_consumer,
+        trigger_rule=TriggerRule.ALL_FAILED,  # Only run if rss fails
+        env_overrides={
+            "SATCONS_SATELLITE": "odegree",
+            "SATCONS_TIME": "{{" \
+                            + "(data_interval_start - macros.timedelta(minutes=210))" \
+                            + ".strftime('%Y-%m-%dT%H:%M')" \
+                            + "}}",
+            "SATCONS_WORKDIR": f"s3://nowcasting-sat-{env}/odegree",
+        },
+        on_failure_callback=slack_message_callback(
+            f"⚠️🇬🇧 The task {get_task_link()} failed to collect odegree satellite data. "
+            "The forecast will automatically move over to PVNET-ECMWF "
+            "which doesn't need satellite data. "
+            "Forecast quality may be impacted, "
+            "but no out-of-hours support is required. "
+            "Please log in an incident log as needed. ",
+        ),
+    )
+
+    extract_latest_odegree_op = extract_latest_zarr(
+        bucket=f"nowcasting-sat-{env}",
+        prefix="odegree/data/odegree_uk3000m.icechunk",
+        window_mins=210,
+        cadence_mins=15,
+    )
 
     latest_only_op >> satip_consume >> update_5min_op >> update_15min_op
     latest_only_op >> consume_rss_op >> extract_latest_rss_op
+    consume_rss_op >> consume_odegree_op >> extract_latest_odegree_op
+
 
 
 @dag(
