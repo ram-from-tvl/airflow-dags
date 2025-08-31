449bb254fecf668c01e9f28b6daced7e12f6dc2b
Peter Dudfield <peter.dudfield@hotmail.com>
2025-08-31 19:36:36 +0100

    Revert "Bump nwp-consumer 1.1.26 -> 1.1.29 (#347)"
    This reverts commit 45613eccc81c7212c6ca3113a6d080014f85032b.

--- src/airflow_dags/dags/uk/consume-nwp-dag.py
+++ src/airflow_dags/dags/uk/consume-nwp-dag.py
@@ -29,7 +29,7 @@ env = os.getenv("ENVIRONMENT", "development")
 nwp_consumer = ContainerDefinition(
     name="nwp-consumer",
     container_image="ghcr.io/openclimatefix/nwp-consumer",
-    container_tag="1.1.29",
+    container_tag="1.1.26",
     container_env={
         "CONCURRENCY": "false",
         "LOGLEVEL": "DEBUG",
